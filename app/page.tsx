df956ea056cae9003abb7a7c87dbd7e5485fc038
Sahi1l-Kumar <2230189@kiit.ac.in>
2025-03-21 17:40:36 +0530

    TailwindCss Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <h1 className="text-3xl text-white font-black">Plant Pulse</h1>;
+  return <h1 className="text-3xl text-primary-500 font-black">Plant Pulse</h1>;
 };
 
 export default Home;
